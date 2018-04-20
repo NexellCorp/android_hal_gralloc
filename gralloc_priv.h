694b1b221b1aebf7fa731912facac535f45e6b11
Sungwoo Park <swpark@nexell.co.kr>
2018-04-20 13:03:17 +0900

    Update gralloc verion to r8p1
    This patch updates gralloc module version r8p1.
    
    Following is main changes.
    - close ion handle after allocation because share_fd is only needed.
    - validation of private_handle_t is changed.
    
    Change-Id: Ib48f9820ffb1ce7704ba72c47b9ab30c154a1930
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- gralloc_priv.h
+++ gralloc_priv.h
@@ -324,9 +324,23 @@ struct private_handle_t
 	{
 		const private_handle_t *hnd = (const private_handle_t *)h;
 
-		if (!h || h->version != sizeof(native_handle) || h->numFds != sNumFds ||
-		        h->numInts != (sizeof(private_handle_t) - sizeof(native_handle)) / sizeof(int) - sNumFds ||
-		        hnd->magic != sMagic)
+		if (!hnd || hnd->version != sizeof(native_handle) || hnd->magic != sMagic)
+		{
+			return -EINVAL;
+		}
+
+		int numFds = sNumFds;
+		int numInts = (sizeof(private_handle_t) - sizeof(native_handle)) / sizeof(int) - sNumFds;
+
+#if GRALLOC_ARM_DMA_BUF_MODULE
+		if (hnd->share_fd < 0)
+		{
+			numFds--;
+			numInts++;
+		}
+#endif
+
+		if (hnd->numFds != numFds || hnd->numInts != numInts)
 		{
 			return -EINVAL;
 		}
